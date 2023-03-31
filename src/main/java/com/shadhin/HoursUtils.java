7e0951aa88b539a50f81a75f7b42be6e3e5ed3d6
md_shadhin <mdshadhinkn@gmail.com>
2023-04-01 00:52:15 +0600

    mutiplay hours tests

--- src/main/java/com/shadhin/HoursUtils.java
+++ src/main/java/com/shadhin/HoursUtils.java
@@ -75,4 +75,23 @@ public class HoursUtils {
 // Format and return the result
         return String.format("%02d.%02d", newHours, newMinutes);
     }
+
+
+    // A method that takes a decimal number of hours and returns a flatten number of hours
+    public static double flattenHours(double hour) {
+        checkHourFormat(hour);
+// Extract the integer and fractional parts of the hours
+        String format = String.format("%.2f", hour);
+        String[] parts = format.split("\\.");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+// Divide the minutes by 60
+        double fracPart = (double) minutes / 60;
+        // Add the hours and the fractional part
+        double floatValue = hours + fracPart;
+        // Round to two decimal places
+        floatValue = Math.round(floatValue * 100.0) / 100.0;
+        // Return the flatten hours
+        return floatValue;
+    }
 }
